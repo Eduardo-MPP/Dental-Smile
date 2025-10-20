203de63f4a67ffa782d2bde150929c56c96d4c5e
Eduardo <paivapingoedu@gmail.com>
2025-10-20 02:50:47 -0500

    Agrego BAckend

---
+++ Backend/src/main/java/com/Clinica/Dental/Smile/DentalSmileApplication.java
@@ -0,0 +1,13 @@
+package com.Clinica.Dental.Smile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalSmileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalSmileApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Clinica/Dental/Smile/DentalSmileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Clinica.Dental.Smile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalSmileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
